2a8ac9fe349f6c5d343268091932eafd0e5d5e89
Danil <tolmachdanil@gmaill.com>
2021-03-09 11:18:35 +0300

    first impl

---
+++ src/main/java/ru/ifmo/backend_2021/IntList.java
@@ -0,0 +1,85 @@
+package ru.ifmo.backend_2021;
+
+import java.util.Iterator;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class IntList implements Iterable<Integer> {
+
+    Node head = new Node();
+    Node tail = new Node();
+
+    static class Node {
+
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int d) {
+            data = d;
+            next = null;
+            prev = null;
+        }
+
+        Node() {
+            data = 0;
+            next = null;
+            prev = null;
+        }
+    }
+
+    public IntList() {
+        head.next = tail;
+        tail.prev = head;
+        insert(-1);
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+
+            private Node current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current.next != null;
+            }
+
+            @Override
+            public Integer next() throws IndexOutOfBoundsException {
+                if (!hasNext()) throw new IndexOutOfBoundsException("End of list.");
+                current = current.next;
+                return current.data;
+            }
+        };
+    }
+
+    public void insert(int data) {
+        var newNode = new Node(data);
+
+        if (tail.prev == head) {
+            tail.prev = newNode;
+            head.next = newNode;
+        } else {
+            var current = tail.prev;
+            current.next = newNode;
+            newNode.prev = current;
+            tail.prev = newNode;
+        }
+
+        head.next.data++;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+
+        forEach(x -> result.append(x).append(' '));
+
+        result.setLength(result.length() - 1);
+        return result.toString();
+
+//        return StreamSupport.stream(spliterator(), false).map(Object::toString)
+//                .collect(Collectors.joining(" "));
+    }
+}
--- src/main/java/ru/ifmo/backend_2021/WordStatIndex.java
+++ src/main/java/ru/ifmo/backend_2021/WordStatIndex.java
@@ -1,7 +1,60 @@
 package ru.ifmo.backend_2021;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class WordStatIndex {
-  public static String getStats(String input) {
-    return "";
-  }
+
+    public static List<String> split(String input) {
+        final List<String> words = new ArrayList<>();
+        final StringBuilder lastWord = new StringBuilder();
+
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c) || c == '-' || c == '\'') {
+                lastWord.append(Character.toLowerCase(c));
+                continue;
+            }
+
+            if (!lastWord.isEmpty()) {
+                words.add(lastWord.toString());
+                lastWord.setLength(0);
+            }
+        }
+
+        return words;
+    }
+
+
+    public static String getStatss(String input) {
+        System.out.println(input.substring(0, 15));
+
+        final Map<String, IntList> counter = new LinkedHashMap<>();
+        int index = 1;
+
+        for (String str : split(input)) {
+            counter.computeIfAbsent(str, k -> new IntList()).insert(index);
+            index++;
+        }
+
+        System.out.println(index);
+
+        StringBuilder result = new StringBuilder();
+        counter.forEach((k, v) -> result.append(k).append(' ').append(v).append('\n'));
+
+        result.setLength(result.length() - 1);
+        return result.toString();
+//        return counter.entrySet().parallelStream()
+//                .map(entry -> entry.getKey() + " " + entry.getValue())
+//                .collect(Collectors.joining("\n"));
+    }
+
+    public static String getStats(String input) {
+        return new f().testInput(input);
+    }
+
+
+    public static void main(String[] args) {
+        var a = getStats("To be, or not to be, that\n is the question:");
+        System.out.print(a);
+    }
 }
