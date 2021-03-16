c6804dc99cf08c7f0f1c3363ccd42242af4ca4a4
likorsi <li_korsi@mail.ru>
2021-03-16 15:37:24 +0300

    add lab 2A

--- src/main/java/ru/ifmo/backend_2021/WordStatIndex.java
+++ src/main/java/ru/ifmo/backend_2021/WordStatIndex.java
@@ -1,7 +1,25 @@
 package ru.ifmo.backend_2021;
 
+import java.util.LinkedHashMap;
+
 public class WordStatIndex {
   public static String getStats(String input) {
-    return "";
+    String[] words = input.toLowerCase().replaceAll("[^a-zA-Zа-яА-Я'\s-]+", " ").split("\s+");
+    LinkedHashMap<String, IntList> dict = new LinkedHashMap<String, IntList>();
+    StringBuilder output = new StringBuilder();
+
+    for (int i =0; i < words.length; i++){
+      if (dict.get(words[i]) == null) {
+        IntList indexes = new IntList();
+        indexes.add(i+1);
+        dict.put(words[i], indexes);
+      } else if (dict.get(words[i]) != null) {
+        dict.get(words[i]).add(i+1);
+      }
+    }
+    for(String key : dict.keySet()) {
+      output.append(key).append(" ").append(dict.get(key).getSize()).append(" ").append(dict.get(key).print()).append("\n");
+    }
+    return output.toString().trim();
   }
 }
