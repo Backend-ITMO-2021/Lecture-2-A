b0becaba9e69fad9e976ba9385502215109e8445
Olezha <44305231+mvgvy@users.noreply.github.com>
2021-03-18 12:01:30 +0300

    White flag

--- src/main/java/ru/ifmo/backend_2021/IntList.java
+++ src/main/java/ru/ifmo/backend_2021/IntList.java
@@ -1,14 +1,13 @@
 package ru.ifmo.backend_2021;
 
-import java.util.*;
+import java.util.Iterator;
 
 public class IntList implements Iterable<Integer> {
-    private int lastLine;
-    private int count;
-    private int iCapacity = 16;
-    private float maxO;
-    private float iFactor;
+    private final int iCapacity = 16;
+    private final float maxO = 0.75f;
+    private final float iFactor = 1.5f;
     private int capacity = iCapacity;
+    private int lastIndex = 0;
     private Integer[] list = new Integer[iCapacity];
     private int size = 0;
 
@@ -16,16 +15,19 @@ public class IntList implements Iterable<Integer> {
         return new Iterator<>() {
             private int index = 0;
             public boolean hasNext() {
+
                 return index != lastIndex;
             }
 
             public Integer next() {
+
                 return list[++index];
             }
         };
     }
 
     public int size() {
+
         return size;
     }
 
--- src/main/java/ru/ifmo/backend_2021/WordStatIndex.java
+++ src/main/java/ru/ifmo/backend_2021/WordStatIndex.java
@@ -17,14 +17,18 @@ public class WordStatIndex {
             String word = words[i];
 
             if (statistic.containsKey(word)) {
+
                 statistic.get(word).add(i + 1);
             } else {
+
                 IntList intList = new IntList();
                 intList.add(i + 1);
                 statistic.put(word, intList);
+
             }
         }
         StringBuilder output = new StringBuilder();
+
         statistic.forEach((key, positions) -> {
             output.append(String.format("%s %d", key, positions.size()));
             for (Integer pos : positions) {
