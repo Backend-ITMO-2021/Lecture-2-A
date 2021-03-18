b8b3af9569148df9f0f97991405516bed35b512b
Olezha <44305231+mvgvy@users.noreply.github.com>
2021-03-18 11:27:46 +0300

    Magay Oleg K33402
    Lab-2-A Done

---
+++ src/main/java/ru/ifmo/backend_2021/IntList.java
@@ -0,0 +1,36 @@
+package ru.ifmo.backend_2021;
+
+import java.util.Iterator;
+
+public class IntList implements  {
+    private int lastLine;
+    private int count;
+    private IntList arr;
+
+    public WordStatIntList(int value, int lastLine) {
+        arr = new IntList(value);
+        this.lastLine = lastLine;
+        this.count = 1;
+    }
+
+    public void add(int position, int lineNum) {
+        count++;
+        if (lastLine != lineNum) {
+            arr.add(position);
+            lastLine = lineNum;
+        }
+    }
+
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append(count).append(' ');
+        for (int i = 0; i < arr.getSize(); i++) {
+            result.append(arr.get(i));
+            if (i + 1 != arr.getSize()) {
+                result.append(' ');
+            }
+        }
+        return result.toString();
+    }
+
+}
--- src/main/java/ru/ifmo/backend_2021/WordStatIndex.java
+++ src/main/java/ru/ifmo/backend_2021/WordStatIndex.java
@@ -1,7 +1,37 @@
 package ru.ifmo.backend_2021;
 
+import java.util.*;
+
 public class WordStatIndex {
-  public static String getStats(String input) {
-    return "";
-  }
+    private static final String symbols = "[^a-zA-Zа-яА-Я\\-'\n ]";
+
+    public static getStats(String input) {
+        section("Class LinkedHashMap");
+
+        final Map<String, IntList> statistic = new Map<>();
+
+        String[] words = input.replaceAll(symbols, " ").toLowerCase().split("\\s+");
+
+        for (int i = 0; i < words.length; i++) {
+
+            String word = words[i];
+
+            if (statistic.containsKey(word)) {
+                statistic.get(word).add(i + 1);
+            } else {
+                IntList intList = new IntList();
+                intList.add(i + 1);
+                statistic.put(word, intList);
+            }
+        }
+        StringBuilder output = new StringBuilder(int capacity);
+        statistic.forEach((key, positions) -> {
+            output.append(String.format("%s %d", key, positions.size()));
+            for (Integer pos : positions) {
+                output.append(String.format(" %d", pos));
+            }
+            output.append('\n');
+        });
+        return output.toString().trim();
+    }
 }
