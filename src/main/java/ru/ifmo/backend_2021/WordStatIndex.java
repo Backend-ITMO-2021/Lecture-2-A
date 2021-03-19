74782a4a03c26762e5260f9657025b6c3b8fde03
heidamn <heidamn@gmail.com>
2021-03-19 22:51:44 +0300

    fixes

--- src/main/java/ru/ifmo/backend_2021/WordStatIndex.java
+++ src/main/java/ru/ifmo/backend_2021/WordStatIndex.java
@@ -5,16 +5,16 @@ import java.util.*;
 public class WordStatIndex {
 
   public static String getStats(String input) {
-    String[] input_array = input.toLowerCase().trim().split(" [.,!?\\s:{}|;\"]+");
+    String[] input_array = input.toLowerCase().trim().split("[.,!?\\s:{}|;\"]+");
     Map<String, IntList> result = new HashMap<>();
 
     for (int i = 0; i < input_array.length; i++) {
       if (!result.containsKey(input_array[i])) {
-        IntList new_info = new IntList(new int[]{1, i});
+        IntList new_info = new IntList(new int[]{1, i+1});
         result.put(input_array[i], new_info);
       } else {
         IntList updated_info = result.get(input_array[i]);
-        updated_info.add(i);
+        updated_info.add(i+1);
         updated_info.set(updated_info.get(0) + 1, 0);
         result.replace(input_array[i], updated_info);
       }
