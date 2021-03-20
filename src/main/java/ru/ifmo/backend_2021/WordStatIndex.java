4e98494eafaf8f3c1ddab5eef60e7551282715f4
ShadowErm <shadow0touch@gmail.com>
2021-03-20 23:15:59 +0300

    add lab2A

--- src/main/java/ru/ifmo/backend_2021/WordStatIndex.java
+++ src/main/java/ru/ifmo/backend_2021/WordStatIndex.java
@@ -1,7 +1,24 @@
 package ru.ifmo.backend_2021;
 
+import java.util.LinkedHashMap;
+
 public class WordStatIndex {
   public static String getStats(String input) {
-    return "";
+    String[] listForWords = input.toLowerCase().replaceAll("[^a-zA-Zа-яА-Я'\\s-]+", " ").split("\\s+");
+    LinkedHashMap<String, IntList> wordState = new LinkedHashMap<String, IntList>();
+    for (int i =0; i < listForWords.length; i++){
+      if (wordState.get(listForWords[i]) == null) {
+        IntList indexes = new IntList();
+        indexes.add(i+1);
+        wordState.put(listForWords[i], indexes);
+      } else if (wordState.get(listForWords[i]) != null) {
+        wordState.get(listForWords[i]).add(i+1);
+      }
+    }
+    StringBuilder output = new StringBuilder();
+    for(String word : wordState.keySet()) {
+      output.append(word).append(" ").append(wordState.get(word).getSize()).append(" ").append(wordState.get(word).print()).append("\n");
+    }
+    return output.toString().trim();
   }
 }
