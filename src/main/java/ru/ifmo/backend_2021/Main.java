1f66a2e76445179b851232b50abb8309b8e12de3
Sergey Polyakov <sergpolyakow@mail.ru>
2021-05-25 15:30:21 +0300

    First working commit

---
+++ src/main/java/ru/ifmo/backend_2021/Main.java
@@ -0,0 +1,12 @@
+package ru.ifmo.backend_2021;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(WordStatIndex.getStats("Monday's child is fair of face.\n" +
+                                                  "|Tuesday's child is full of grace.\n" +
+                                                  "|"
+    ));
+    }
+
+}
--- src/main/java/ru/ifmo/backend_2021/WordStatIndex.java
+++ src/main/java/ru/ifmo/backend_2021/WordStatIndex.java
@@ -1,7 +1,24 @@
 package ru.ifmo.backend_2021;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+
 public class WordStatIndex {
   public static String getStats(String input) {
-    return "";
+    LinkedHashMap<String, LinkedList<Integer>> counter = new LinkedHashMap<>();
+    String[] words = input.toLowerCase().replaceAll("( )+", " ").strip().split("[^\\p{L}'\\-]+");
+    for (int i = 0; i < words.length; i++) {
+      counter.putIfAbsent(words[i], new LinkedList<>());
+      counter.get(words[i]).add(i + 1);
+    }
+
+    StringBuilder stats = new StringBuilder("");
+    counter.forEach((word, list) -> {
+      stats.append(word).append(" ").append(list.size());
+      list.forEach(i-> stats.append(" ").append(i));
+      stats.append("\n");
+    });
+    stats.setLength(stats.length() - 1);
+    return stats.toString();
   }
 }
